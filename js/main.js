47252f04a2d0ad22d9e2eb1c490ddb1805c39cfb
Eric <Ericrius1@gmail.com>
2014-06-26 22:34:55 +0200

    yeah

--- js/main.js
+++ js/main.js
@@ -3,39 +3,21 @@
 
 //FIGURE OUT SCREENSPACE TO WORLD SPACE!!
 
-var camera, renderer, projector, scene, controls, clock, perlin, wineglass, brushtray, landscape, sky, village;
+var camera, renderer, projector, scene, controls, clock;
 var particleGroup;
 var line;
 var randFloat = THREE.Math.randFloat;
 
-var sphere;
 
 window.addEventListener('resize', onWindowResize);
 
 var w = window.innerWidth;
 var h = window.innerHeight;
-var leftScreen = -w/16;
-var rightScreen = w/16;
-console.log(window.innerWidth)
-var topScreen = 50;
-var bottomScreen = -50;
-
-
-var csd = {
-  volume: 0.0
-};
-var fsd = {
-  volume: 1.0
-}
-var audio = loadAudio('assets/godfather.mp3');
-audio.volume = 0;
-
-
-
 
 
 
 init();
+animate();
 function init() {
   clock = new THREE.Clock();
 
@@ -47,50 +29,25 @@ function init() {
 
   scene = new THREE.Scene();
 
-  renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: false});
-  // renderer.autoClearColor = false;
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(w, h);
   document.body.appendChild(renderer.domElement);
 
 }
 
-function start() {
-
-  brushFactory = new BrushFactory();
-  landscape = new Landscape();
-  audio.play();
-  var audioTween = new TWEEN.Tween(csd).
-    to(fsd, 5000).
-    easing(TWEEN.Easing.Cubic.In).
-    onUpdate(function(){
-      audio.volume = csd.volume;
-  }).start();
-  animate();
-
-}
-
-
 function animate() {
   TWEEN.update();
-  landscape.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
-
 
 function map(value, min1, max1, min2, max2) {
   return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
 };
 
 function onWindowResize() {
-  // renderer.setSize(window.innerWidth, window.innerHeight);
-  // camera.aspect = window.innerWidth / window.innerHeight;
-  // camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 }
 
-
-function loadAudio(uri) {
-  var audio = new Audio();
-  audio.src = uri;
-  return audio;
-}
